81c7e58590f81fbe81ece910523e2c66c869e5f1
Pakkapon Phongtawee <me@pureapp.in.th>
2016-03-30 20:23:43 +0700

    debug

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
+  $("#login-btn").click(function(){
+    FB.login(function(response) {
+      if (response.authResponse) {
+        console.log('Welcome!  Fetching your information.... ');
+        FB.api('/me', function(response) {
+          console.log('Good to see you, ' + response.name + '.');
+          console.log(response);
+        });
+      } else {
+        console.log('User cancelled login or did not fully authorize.');
+      }
+    }, {scope: 'email,public_profile,user_friends'});
+  })
   $("html").on("fbLoaded",function(){
     FB.getLoginStatus(function(response) {
       if (response.status === 'connected') {
