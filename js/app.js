262ec43d462981f4d7b965e78ab032cbd9d5d6a7
Pakkapon Phongtawee <me@pureapp.in.th>
2016-03-31 19:08:19 +0700

    fix try catch

--- js/app.js
+++ js/app.js
@@ -6,20 +6,24 @@ $(document).ready(function(){
         FB.api('/me?fields=email,name,first_name,last_name', function(response) {
           localStorage.player = response.id
           date = (new Date()).toISOString();
-          $.post(apiServer+"/Players", {
-            id: response.id,
-            name: response.name,
-            firstname: response.first_name,
-            lastname:response.last_name,
-            email:response.email,
-            updated_time:date
-          }).done(function() {
-            localStorage.player = response.id
-            window.location="thread.html";
-          }).fail(function() {
-            localStorage.player = response.id
-            window.location="thread.html";
-          });
+          try{
+            $.post(apiServer+"/Players", {
+              id: response.id,
+              name: response.name,
+              firstname: response.first_name,
+              lastname:response.last_name,
+              email:response.email,
+              updated_time:date
+            }).done(function() {
+              localStorage.player = response.id
+              window.location="thread.html";
+            }).fail(function() {
+              localStorage.player = response.id
+              window.location="thread.html";
+            });
+          catch(e){
+            // do nothing
+          }
         });
       } else {
         console.log('User cancelled login or did not fully authorize.');
